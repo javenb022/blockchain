614adac547716965e3e95f8d0caa68e46b8ace36
Javen Wilson <javenb022@gmail.com>
2024-07-29 23:10:15 -0400

    Creates the Block class

--- block.rb
+++ block.rb
@@ -1,23 +1,25 @@
-require 'digest'
+require "digest"
+require "pp"
+require "pry"
 
 class Block
-  attr_accessor :data, :previous_hash, :hash, :nonce, :index, :timestamp
+  attr_reader :data, :hash
+  attr_accessor :prev_hash, :nonce # Creates setter method to re-assign this value in the method in Blockchain
 
-  def initialize(data, previous_hash)
+  def initialize(data, prev_hash = "")
     @data = data
-    @timestamp = Time.now
-    @index = 0
-    @previous_hash = previous_hash
+    @prev_hash = prev_hash
     @hash = calculate_hash
     @nonce = 0
   end
 
   def calculate_hash
-    Digest::SHA256.hexdigest(@index.to_s + @timestamp.to_s + @previous_hash + @data.to_s + @nonce.to_s)
+    Digest::SHA256.hexdigest("#{nonce}#{prev_hash}#{data}")
   end
 
-  def mine_block(difficulty)
-    while @hash[0...difficulty] != "0" * difficulty
+  def mine_block(num_zeros)
+    pattern = '0' * num_zeros
+    while @hash[0, num_zeros] != pattern
       @nonce += 1
       @hash = calculate_hash
     end
